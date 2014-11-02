384cb778799c4afd308ef821a0891fe8bffcfbad
Tristan Smith <darcnite3000@gmail.com>
2014-11-03 10:05:31 +1100

    updated sass task to include bourbon

--- gulp/index.js
+++ gulp/index.js
@@ -2,6 +2,6 @@ var gulp        = require('gulp'),
     requireDir  = require('require-dir'),
     tasks       = requireDir('./tasks');
 
-gulp.task('default',['browserify','sass']);
+gulp.task('default',['browserify','sass','vendor']);
 
 module.exports = gulp;
--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,13 +1,21 @@
-var gulp = require('gulp'),
-    sourcemaps = require('gulp-sourcemaps'),
-    sass = require('gulp-sass'),
-    config = require('../config.json').styles;
+var gulp          = require('gulp'),
+    sourcemaps    = require('gulp-sourcemaps'),
+    sass          = require('gulp-sass'),
+    autoprefixer  = require('gulp-autoprefixer'),
+    config        = require('../config.json').styles;
 
 gulp.task('sass', function() {
   return gulp
     .src(config.files)
     .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(sass())
-    .pipe(sourcemaps.write('./'))
+    .pipe(sass({
+      includePaths: require('node-bourbon').includePaths,
+      errLogToConsole: true
+    }))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write('./',{
+      includeContent:false,
+      sourceRoot: '/styles'
+    }))
     .pipe(gulp.dest(config.build));
 });
