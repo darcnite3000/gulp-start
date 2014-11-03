be1c12250c6620df5f826d1864074a3e0c1dedaa
Tristan Smith <darcnite3000@gmail.com>
2014-11-03 17:13:21 +1100

    fix: styles instead of scripts for sass

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,9 +1,10 @@
+'use strict';
 var gulp        = require('gulp'),
     livereload  = require('gulp-livereload'),
     config      = require('../config.json');
 
-gulp.task('watch',['watchify','sass'], function() {
+module.exports = gulp.task('watch',['watchify','sass'], function() {
   livereload.listen();
   gulp.watch('public/**').on('change',livereload.changed);
-  gulp.watch(config.scripts.files, ['sass']);
+  gulp.watch(config.styles.files, ['sass']);
 });
