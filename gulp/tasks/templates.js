1b8d8885ad08a548cd77323543834439ead18683
Tristan Smith <darcnite3000@gmail.com>
2014-11-10 11:26:37 +1100

    added angular template creation task

---
+++ gulp/tasks/templates.js
@@ -0,0 +1,18 @@
+'use strict';
+var gulp        = require('gulp'),
+    templates   = require('gulp-angular-templatecache'),
+    minhtml     = require('gulp-minify-html'),
+    config      = require('../config.json').scripts;
+
+module.exports = gulp.task('templates', function() {
+    if(config.angular){
+      gulp.src(config.templates.src)
+        .pipe(minhtml())
+        .pipe(templates(config.templates.name+".js"),{
+          module: config.templates.name,
+          standalone: true,
+          moduleSystem: 'Browserify'
+        })
+        .pipe(gulp.dest(config.templates.dest));
+    }
+});
